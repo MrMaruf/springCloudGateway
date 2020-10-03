a1c39cab94301d0082275d1cda28a56f1133073d
MrMaruf <58888354+MrMaruf@users.noreply.github.com>
2020-10-03 23:04:52 +0200

    Update to gateway to work with db

--- src/main/java/maruf/services/gateway/GatewayServiceApplication.java
+++ src/main/java/maruf/services/gateway/GatewayServiceApplication.java
@@ -2,37 +2,33 @@ package maruf.services.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@Controller
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-															ReactiveClientRegistrationRepository clientRegistrationRepository) {
-		// Authenticate through configured OpenID Provider
-		http.oauth2Login();
-
-		// Also logout at the OpenID Connect provider
-		http.logout(logout -> logout.logoutSuccessHandler(
-				new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
-
-		// Require authentication for all requests
-		http.authorizeExchange().anyExchange().authenticated();
-
-		// Allow showing /home within a frame
-		http.headers().frameOptions().mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
-
-		// Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-		http.csrf().disable();
-		return http.build();
+	@GetMapping("/whoami")
+	@ResponseBody
+	public Map<String, Object> index(
+			@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+			@AuthenticationPrincipal OidcUser oidcUser) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("clientName", authorizedClient.getClientRegistration().getClientName());
+		model.put("userName", oidcUser.getName());
+		model.put("userAttributes", oidcUser.getAttributes());
+		return model;
 	}
 }
